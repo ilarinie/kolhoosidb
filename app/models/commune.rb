5f578b7cc06d4556012b5d1792fc62f1ce983818
ilarinie <ilari.nieminen@outlook.com>
2017-06-05 19:58:44 +0300

    tein jotain

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -2,4 +2,8 @@ class Commune < ApplicationRecord
   has_many :users, through: :commune_users
   has_many :purchases
   has_many :tasks
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 5 }
+
 end
