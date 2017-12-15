e4356a0025bfd600b9bb85eb4e7c27c476a8b6d7
Ilari Nieminen <ilari.nieminen@outlook.com>
2017-12-15 22:19:16 +0200

    m

--- deploy_integration.sh
+++ deploy_integration.sh
@@ -3,9 +3,7 @@ eval "$(ssh-agent -s)}"
 chmod 600 ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 
-git config --global push.default matching
-git reset --hard HEAD
-git remote add deploy ssh://ile@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://kommuuniapp@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 ssh ile@$IP -p $PORT <<EOF
