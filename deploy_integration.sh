e07df188a359d792899c313d40a10fda1b7a203f
Ilari Nieminen <ilari.nieminen@outlook.com>
2017-12-16 10:38:58 +0200

    m

--- deploy_integration.sh
+++ deploy_integration.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 eval "$(ssh-agent -s)}"
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
 
 git remote add deploy ssh://kommuuniapp@$IP:$PORT$DEPLOY_DIR
-git push deploy
+git push deploy master
 
 ssh kommuuniapp@$IP -p $PORT -o StrictHostKeyChecking=no<<EOF
-    yes
     cd $DEPLOY_DIR
     rake db:migrate
 EOF
