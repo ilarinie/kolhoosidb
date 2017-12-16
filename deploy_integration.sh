6409c83db5de0578c00d6ef8d77411d788c852b2
Ilari Nieminen <ilari.nieminen@outlook.com>
2017-12-17 00:57:59 +0200

    m

--- deploy_integration.sh
+++ deploy_integration.sh
@@ -5,7 +5,4 @@ chmod 700 ~/.ssh/
 chmod 600 ~/.ssh/id_rsa
 rm ~/.ssh/known_hosts
 
-git remote add deploy ssh://kommuuniapp@$IP:$PORT$DEPLOY_DIR
-git push deploy dev
-
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" kommuuniapp@$IP -p $PORT deploy_integration.sh
