7884cd2f7c6bae69e14c0f4385ae6497a4c847cb
Ilari Nieminen <ilari.nieminen@outlook.com>
2017-12-17 01:08:35 +0200

    m

--- deploy_integration.sh
+++ deploy_integration.sh
@@ -3,4 +3,4 @@ chmod 700 ~/.ssh/
 chmod 600 ~/.ssh/id_rsa
 rm ~/.ssh/known_hosts
 
-ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" kommuuniapp@$IP -p $PORT /home/kommuuniapp/bin/deploy_integration.sh
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" kommuuniapp@$IP sh /home/kommuuniapp/bin/deploy_integration.sh
