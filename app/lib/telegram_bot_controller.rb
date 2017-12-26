4d659a0e3420d17eb63e20613f4c07e05e8348cd
ilarinie <ilari.nieminen@outlook.com>
2017-12-26 17:10:26 +0200

    Improved telegram api error handling

--- app/lib/telegram_bot_controller.rb
+++ app/lib/telegram_bot_controller.rb
@@ -5,11 +5,18 @@ class TelegramApi
 
 
   def self.send_to_channel commune, message
-    if not ENV["TELEGRAM_BOT_TOKEN"].nil? && commune.telegram_channel_token.nil?
-      @token =  ENV["TELEGRAM_BOT_TOKEN"]
-      Telegram::Bot::Client.run(@token) do |bot|
-        bot.api.send_message(chat_id: commune.telegram_channel_token, text: message)
-      end
+    
+    if commune.telegram_channel_token.nil?
+      return
+    end
+
+    if ENV["TELEGRAM_BOT_TOKEN"].nil?
+      return
+    end
+
+    @token =  ENV["TELEGRAM_BOT_TOKEN"]
+    Telegram::Bot::Client.run(@token) do |bot|
+      bot.api.send_message(chat_id: commune.telegram_channel_token, text: message)
     end
   end
 end
