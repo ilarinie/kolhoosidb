37135ce425e5d3f44163532a49139a6706869a27
ilarinie <ilari.nieminen@outlook.com>
2017-12-28 21:29:33 +0200

    Continued adding logs, must figure out current_user.

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -1,6 +1,6 @@
 class Commune < ApplicationRecord
-  include PublicActivity::Model
-  tracked owner: Proc.new{ |controller, model| controller.current_user}, recipient: Proc.new{ |controller, model| controller.find_commune_and_check_if_admin }
+  #include PublicActivity::Model
+  #tracked owner: Proc.new{ |controller, model| controller.current_user}, recipient: Proc.new{ |controller, model| controller.find_commune_and_check_if_admin }
 
   attr_accessor :commune
 
@@ -18,10 +18,37 @@ class Commune < ApplicationRecord
   validates :name, presence: true, length: {min:2, maximum: 30}
   validates :name, length: { maximum: 30 }
 
+  after_create :log_create
+  after_update :log_update
+
   self.primary_key = 'id'
 
   def is_admin user
     return self.admins.include? user
   end
 
+  def log_create
+    message = "#{self.owner.name} created #{self.name}!"
+    CommuneLog.create(commune_id: self.id, message: message, long_message: "")
+  end
+
+  def log_update
+    message = "#{get_current_user_name} edited the commune details."
+    long_message = ""
+    if self.name_changed?
+      long_message = long_message + "Old name: #{self.name_was}, new name: #{self.name}"
+    end
+    if self.description_changed?
+      long_message = long_message + "Old description: '#{self.description_was}', new description '#{self.description}'"
+    end
+    CommuneLog.create(commune_id: self.id, message: message, long_message: long_message)
+  end
+
+  def get_current_user_name
+    if defined?(current_user)
+      return  current_user.name
+    else
+      return ""
+    end
+  end
 end
--- app/models/commune_admin.rb
+++ app/models/commune_admin.rb
@@ -1,4 +1,10 @@
 class CommuneAdmin < ApplicationRecord
   belongs_to :commune
   belongs_to :user
+  after_save :log_create
+
+  def log_create
+    message = "#{self.user.name} was promoted to admin by #{current_user.name}."
+    CommuneLog.create(commune_id: self.commune_id, message: message, long_message: "")
+  end
 end
