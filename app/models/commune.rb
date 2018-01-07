645ae0c75473085841bc90d8ca60bc5d8e9cd9c5
ilarinie <ilari.nieminen@outlook.com>
2018-01-07 20:23:42 +0200

    Improved purchase listing (includes users and correct order), added awesome print.

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -1,6 +1,6 @@
 class Commune < ApplicationRecord
   include PublicActivity::Model
-  tracked owner: Proc.new{ |controller, model| controller.current_user}, recipient: Proc.new{ |controller, model| controller.find_commune_and_check_if_admin }
+  # tracked owner: Proc.new{ |controller, model| controller.current_user}, recipient: Proc.new{ |controller, model| controller.find_commune_and_check_if_admin }
 
   attr_accessor :commune
 
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,5 +1,15 @@
-users = 2
+users = 3
+
+
 
 (1..users).each do |i|
   User.create! name: "testaaja#{i}", username: "testaaja#{i}", email: "test@test.fi", password: "testaaja", password_confirmation: "testaaja"
 end
+
+@commune = Commune.create!(owner: User.first, name: 'Test commune', description: 'test description')
+
+@commune.admins << User.first
+@commune.users << User.second
+@commune.users << User.last
+
+PurchaseCategory.create!(commune_id: Commune.first.id, name: 'Default')
