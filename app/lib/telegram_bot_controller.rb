9d2e434bfb38c3cd8e6e33333a99e680a138e08a
ilarinie <ilari.nieminen@outlook.com>
2018-01-16 19:39:40 +0200

    Tasks no longer trigger notification on telegram channel.

--- app/lib/telegram_bot_controller.rb
+++ app/lib/telegram_bot_controller.rb
@@ -1,10 +1,7 @@
 class TelegramApi
-  require 'telegram/bot'
+  require "telegram/bot"
 
-
-
-
-  def self.send_to_channel commune, message
+  def self.send_to_channel (commune, message, no_notification)
 
     if commune.telegram_channel_token.nil?
       return
@@ -13,10 +10,9 @@ class TelegramApi
     if ENV["TELEGRAM_BOT_TOKEN"].nil?
       return
     end
-
     @token =  ENV["TELEGRAM_BOT_TOKEN"]
     Telegram::Bot::Client.run(@token) do |bot|
-      bot.api.send_message(chat_id: commune.telegram_channel_token, text: message)
+      bot.api.send_message(chat_id: commune.telegram_channel_token, text: message, disable_notification: no_notification)
     end
   end
 end
